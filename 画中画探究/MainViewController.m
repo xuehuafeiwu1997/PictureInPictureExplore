ff0c3adc755a1a1e1e11942287131e25910f8721
xumingyang <xumingyang@greatyun.com>
2020-10-30 11:47:11 +0800

    整理代码

--- 画中画探究/MainViewController.m
+++ 画中画探究/MainViewController.m
@@ -10,7 +10,9 @@
 #import "FirstViewController.h"
 #import "SecondViewController.h"
 
-@interface MainViewController ()
+@interface MainViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -18,18 +20,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     self.title = @"主界面";
     self.navigationController.navigationBar.translucent = NO;
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.tableView];
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-//    FirstViewController *vc = [[FirstViewController alloc] init];
-//    ViewController *vc = [[ViewController alloc] init];
-    SecondViewController *vc = [[SecondViewController alloc] init];
-    [self.navigationController pushViewController:vc animated:YES];
+#pragma mark - delegate/dataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    if (indexPath.row == 0) {
+        cell.textLabel.text = @"AVPlayerController播放视频，实现画中画功能";
+    } else {
+        cell.textLabel.text = @"AVPlayerLayer播放视频，实现画中画功能";
+    }
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (indexPath.row == 0) {
+        FirstViewController *vc = [[FirstViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+    if (indexPath.row == 1) {
+        SecondViewController *vc = [[SecondViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 100;
+}
+
+- (UITableView *)tableView {
+    if (_tableView) {
+        return _tableView;
+    }
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStylePlain];
+    _tableView.backgroundColor = [UIColor clearColor];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    return _tableView;
 }
 
 @end
