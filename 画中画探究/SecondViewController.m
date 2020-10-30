bd55298e40c486e9560084cc192d84877b256a51
xumingyang <xumingyang@greatyun.com>
2020-10-30 11:14:26 +0800

    AVPlayerLayer实现画中画功能

--- 画中画探究/SecondViewController.m
+++ 画中画探究/SecondViewController.m
@@ -9,12 +9,14 @@
 #import <AVKit/AVKit.h>
 #import "PlayerView.h"
 
-@interface SecondViewController ()
+@interface SecondViewController ()<AVPictureInPictureControllerDelegate>
 
 @property (nonatomic, strong) AVPlayer *player;
 @property (nonatomic, strong) PlayerView *playerView;
 @property (nonatomic, strong) UIButton *playButton;
 @property (nonatomic, strong) UIButton *pauseButton;
+@property (nonatomic, strong) UIButton *startPipButton;
+@property (nonatomic, strong) AVPictureInPictureController *pipVC;
 
 @end
 
@@ -23,6 +25,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    if ([AVPictureInPictureController isPictureInPictureSupported]) {
+        NSLog(@"该设备支持画中画功能");
+        //开启画中画权限
+        NSError *error = nil;
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&error];
+        if (error) {
+            NSLog(@"请求权限失败的原因为%@",error);
+        }
+    } else {
+        NSLog(@"该设备不支持画中画功能");
+    }
     self.title = @"AVPlayerLayer播放视频实现画中画功能";
     self.navigationController.navigationBar.translucent = NO;
     self.view.backgroundColor = [UIColor whiteColor];
@@ -32,6 +45,8 @@
     [self.view addSubview:self.playButton];
     self.pauseButton.frame = CGRectMake((self.view.bounds.size.width - 100) / 2, 400, 100, 50);
     [self.view addSubview:self.pauseButton];
+    self.startPipButton.frame = CGRectMake((self.view.bounds.size.width - 200) / 2, 460, 200, 50);
+    [self.view addSubview:self.startPipButton];
 }
 
 - (void)playVideo {
@@ -42,6 +57,16 @@
 - (void)pauseVideo {
     NSLog(@"暂停播放视频");
     [self.playerView.player pause];
+}
+
+- (void)startPip {
+    NSLog(@"点击开启画中画功能");
+    if (self.pipVC.pictureInPicturePossible) {
+        NSLog(@"允许开启画中画功能");
+        [self.pipVC startPictureInPicture];
+    } else {
+        NSLog(@"不允许开启画中画功能");
+    }
 }
 
 - (AVPlayer *)player {
@@ -81,6 +106,47 @@
     [_pauseButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [_pauseButton addTarget:self action:@selector(pauseVideo) forControlEvents:UIControlEventTouchUpInside];
     return _pauseButton;
+}
+
+- (UIButton *)startPipButton {
+    if (_startPipButton) {
+        return _startPipButton;;
+    }
+    _startPipButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+    [_startPipButton setTitle:@"开启画中画功能" forState:UIControlStateNormal];
+    [_startPipButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [_startPipButton addTarget:self action:@selector(startPip) forControlEvents:UIControlEventTouchUpInside];
+    return _startPipButton;
+}
+
+- (AVPictureInPictureController *)pipVC {
+    if (_pipVC) {
+        return _pipVC;
+    }
+    _pipVC = [[AVPictureInPictureController alloc] initWithPlayerLayer:self.playerView.playerLayer];
+    _pipVC.delegate = self;
+    return _pipVC;
+}
+
+#pragma mark - AVPictureInPictureControllerDelegate
+- (void)pictureInPictureControllerWillStartPictureInPicture:(AVPictureInPictureController *)pictureInPictureController {
+    NSLog(@"即将开启画中画功能");
+}
+
+- (void)pictureInPictureControllerDidStartPictureInPicture:(AVPictureInPictureController *)pictureInPictureController {
+    NSLog(@"已经开启画中画功能");
+}
+
+- (void)pictureInPictureControllerWillStopPictureInPicture:(AVPictureInPictureController *)pictureInPictureController {
+    NSLog(@"即将停止画中画功能");
+}
+
+- (void)pictureInPictureControllerDidStopPictureInPicture:(AVPictureInPictureController *)pictureInPictureController {
+    NSLog(@"已经停止画中画功能");
+}
+
+- (void)pictureInPictureController:(AVPictureInPictureController *)pictureInPictureController failedToStartPictureInPictureWithError:(NSError *)error {
+    NSLog(@"开启画中画功能失败，原因是%@",error);
 }
 
 @end
